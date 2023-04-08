920749fddd97c6e2728d6b76a17b2d9cc85f3c65
Roman Kadevich <romankadevich@gmail.com>
2023-04-08 12:05:00 +0300

    add burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,6 +1,18 @@
-window.addEventListener('DOMContentLoaded',()=>{
+window.addEventListener('DOMContentLoaded', () => {
+    let menu = document.getElementById('menu')
+    let burger = document.getElementById('burger')
+    let screen = document.getElementById('screen')
+    burger.addEventListener('click', function () {
+        menu.classList.toggle('nav-mob--active');
+        burger.classList.toggle('nav__burger--active')
+        screen.classList.toggle('screen-active')
+    })
+    document.querySelector('#screen').addEventListener('click',function(){
+         menu.classList.remove('nav-mob--active');
+         screen.classList.remove('screen-active');
+       
+     })
 
-
-
-    
 })
+
+

8814a7e066114ea0545c645b122352b850125c75
RomanKadevich <romankadevich@gmail.com>
2023-06-10 16:47:34 +0300

    feat: migrate news and sources js files to ts

---
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -0,0 +1,56 @@
+import './news.css';
+import { Source } from '../../../types/index';
+
+class News {
+    draw(data: Source[]): void {
+        const news: Source[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment: DocumentFragment = document.createDocumentFragment() as DocumentFragment;
+        const newsItemTemp: HTMLTemplateElement | null = document.querySelector<HTMLTemplateElement>('#newsItemTemp');
+        if (newsItemTemp) {
+            news.forEach((item, idx) => {
+                const newsClone: DocumentFragment = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+                const newsItem: Element | null = newsClone.querySelector('.news__item');
+                if (newsItem) {
+                    if (idx % 2) newsItem.classList.add('alt');
+                }
+                const newsMetaPhoto: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
+                if (newsMetaPhoto) {
+                    newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+                }
+                const newsMetaAuthor: Element | null = newsClone.querySelector('.news__meta-author');
+                if (newsMetaAuthor) {
+                    newsMetaAuthor.textContent = item.author || item.source.name;
+                }
+                const newsMetaDate: Element | null = newsClone.querySelector('.news__meta-date');
+                if (newsMetaDate) {
+                    newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+                }
+                const newsDescriptionTitle = newsClone.querySelector('.news__description-title');
+                if (newsDescriptionTitle) {
+                    newsDescriptionTitle.textContent = item.title;
+                }
+                const newsDescriptionSource = newsClone.querySelector('.news__description-source');
+                if (newsDescriptionSource) {
+                    newsDescriptionSource.textContent = item.source.name;
+                }
+                const newsDescriptionContent = newsClone.querySelector('.news__description-content');
+                if (newsDescriptionContent) {
+                    newsDescriptionContent.textContent = item.description;
+                }
+                const newsReadMore = newsClone.querySelector('.news__read-more a');
+                if (newsReadMore) {
+                    newsReadMore.setAttribute('href', item.url);
+                }
+                fragment.append(newsClone);
+            });
+            const newsEl = document.querySelector('.news');
+            if (newsEl) {
+                newsEl.innerHTML = '';
+                newsEl.appendChild(fragment);
+            }
+        }
+    }
+}
+
+export default News;
---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+    urlToImage: string;
+    source: { name: string };
+    author: string;
+    publishedAt: string;
+    title: string;
+}

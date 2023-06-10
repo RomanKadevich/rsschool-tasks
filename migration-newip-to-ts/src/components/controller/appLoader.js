5af432dca393cc1cfd8cd2c1c680600608c7046a
RomanKadevich <romankadevich@gmail.com>
2023-06-10 11:05:29 +0300

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2a60d8b800de4840b6c0198f374881d4', // получите свой ключ https://newsapi.org/
         });
     }
 }

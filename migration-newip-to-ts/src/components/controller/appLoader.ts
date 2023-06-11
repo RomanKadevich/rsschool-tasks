57e70cb1bfc77d1a1e1144babb598aff51792c6c
RomanKadevich <romankadevich@gmail.com>
2023-06-11 20:03:35 +0300

    fix: ts files

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '2a60d8b800de4840b6c0198f374881d4', // получите свой ключ https://newsapi.org/
         });
     }

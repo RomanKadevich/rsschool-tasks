82cf17ed33ffae2c9ff661d130fb73676925b012
RomanKadevich <romankadevich@gmail.com>
2023-06-11 21:27:03 +0300

    refactor: css files

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://rss-news-api.onrender.com/', {
-            apiKey: '2a60d8b800de4840b6c0198f374881d4', // получите свой ключ https://newsapi.org/
+            apiKey: '68ad981c4ebe428fb4a54ba2c3fb9aa6', // получите свой ключ https://newsapi.org/
         });
     }
 }

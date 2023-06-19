7f799cbcce2ead8e1d4512cc7b3f844e14fdda3d
RomanKadevich <romankadevich@gmail.com>
2023-06-19 21:58:28 +0300

    feat: create a structute of the project

---
+++ RSS-CSS-Selectors/src/app/app.ts
@@ -0,0 +1,12 @@
+export class App{
+    private static container: HTMLElement = document.body;
+    
+    constructor(){
+        // this.startPage = new FirstLevel();
+    }
+
+    run(){
+
+    }
+}
+
